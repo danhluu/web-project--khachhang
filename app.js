f77e71afc702c14381fe98221af6afe65e08ca4a
Lê Danh Lưu <danhluu1@gmail.com>
2020-12-24 17:30:38 +0700

    passport

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 var hbs=require('hbs');
 const { MongoClient } = require("mongodb");
-
+var passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var productsRouter=require('./routes/products');
 var userRouter=require('./routes/user');
+
+const { RequestTimeout } = require('http-errors');
 require('./db/db.js');
-//var productdetailRouter=require('./routes/productdetail');
 
 var app = express();
 
@@ -22,16 +27,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(path.join(__dirname,'/views/partials'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.bodyParser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({secret: "laksjdoiwahfalsnc21983ulkasdn", resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+require('./utils/passport')(passport);
+
+
 app.use('/', indexRouter);
 app.use('/products',productsRouter);
 app.use('/user',userRouter);
-//app.use('/productdetail',productdetailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
-var homeController=require('../controllers/homeController');
-
+var homeController = require('../controllers/homeController');
 
 router.get('/', homeController.index);
+router.get('/login',homeController.login);
+router.get('/signup', homeController.signup);
+
+router.post('/login',passport.authenticate('local-login', { 
+                    successRedirect: '/',
+                    failureRedirect: '/login',
+                    failureFlash: true
+                   })
+);
+router.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/',
+    failureRedirect: '/signup',
+    failureFlash: true,
+  }));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
 
 module.exports = router;
